300340390c24c642c99ccd869cf53e1113910053
SylphicXen <sylphicxen@sylphicxen.xyz>
2023-03-01 00:08:39 -0600

    Set deployment to global for slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -25,7 +25,8 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
         console.log(`Started refreshing ${commands.length} application (/) commands.}`);
 
         const data = await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.DEV_GUILD_ID),
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            // Routes.applicationCommands(process.env.CLIENT_ID, process.env.DEV_GUILD_ID),
             { body: commands },
         );
 
